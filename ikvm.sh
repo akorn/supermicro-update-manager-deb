0d076fcbcca7b656b0a77d33beaab4654d1c3038
Andrey Domas <andrey.domas@odnoklassniki.ru>
2017-03-29 15:59:22 +0300

    ikvm.sh improvements

--- ikvm.sh
+++ ikvm.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-if [ "$3" == "" ];
-then
-	echo "Usage: $0 <ip> <username> <password>"
-	exit
-fi;
+CONF="/etc/ikvm.conf"
 
-/usr/bin/java -Djava.library.path=/usr/lib/ipmiview/ -jar /usr/lib/ipmiview/iKVM.jar "$1" "$2" "$3" null 5900 623 2 0 >/dev/null
+if ! [[ -r "${CONF}" ]]; then
+    echo "Create file /etc/ikvm.conf with the content:"
+    echo "USERNAME=Administrator"
+    echo "PASSWORD_DEFAULT=secret"
+    exit
+fi
+
+. ${CONF}
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <hostname>"
+    exit
+fi
+
+if [[ "$1" == ${PATTERN_1} ]]; then
+    password="${PASSWORD_1}"
+else
+    password="${PASSWORD_DEFAULT}"
+fi
+
+/usr/bin/java -Djava.library.path=/usr/lib/ipmiview/ -jar /usr/lib/ipmiview/iKVM.jar "${1}-ilo" "${USERNAME}" "${password}" null 5900 623 2 0 >/dev/null
